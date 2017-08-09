8ef163c7cda8e96c0ce1ec9fb4ea17f1bd2325a2
hushaoqing <hu253955106@163.com>
2017-08-09 15:55:11 +0800

    Python : add Tornado; update PythonTips/A.py

--- Python/PythonTips/A.py
+++ Python/PythonTips/A.py
@@ -1,7 +1,7 @@
 # !usr/bin/python
 # coding=utf-8
 
-
+# ? colsure
 def function1():
     co = [0]
     def function3():
@@ -15,6 +15,8 @@ def function1():
 if __name__ == '__main__':
     print function1() # 2
 
+
+# lazyproperty
 class lazyproperty(object):
     """docstring for lazyproperty"""
     def __init__(self, func):
@@ -69,3 +71,132 @@ if __name__ == '__main__':
 # {'area': 50.26548245743669, 'arg': 4}
 # 50.2654824574
 # [Finished in 0.0s]
+
+# lazy calculate
+import sys
+l = [x for x in range(100)]
+ll= (x for x in range(100))
+print sys.getsizeof(l), type(l)
+print sys.getsizeof(ll), type(ll)
+
+def lazymult(a,b):
+    return lambda: a*b
+def lazyfac (n):
+    if n == 0:
+        return lambda : 1
+    else:
+        return lambda: lazymult(n, lazyfac(n-1)())()
+a=lazyfac(5)
+print a
+print a()
+
+
+
+# tornado async crawler
+import time
+from datetime import timedelta
+
+try:
+    from HTMLParser import HTMLParser
+    from urlparse import urljoin, urldefrag
+except ImportError:
+    from html.parser import HTMLParser
+    from urllib.parse import urljoin, urldefrag
+
+from tornado import httpclient, gen, ioloop, queues
+
+base_url = 'http://www.tornadoweb.org/en/stable/'
+concurrency = 10
+
+@gen.coroutine
+def get_links_from_url(url):
+    """Download the page at `url` and parse it for links.
+
+    Returned links have had the fragment after `#` removed, and have been made
+    absolute so, e.g. the URL 'gen.html#tornado.gen.coroutine' becomes
+    'http://www.tornadoweb.org/en/stable/gen.html'.
+    """
+    try:
+        response = yield httpclient.AsyncHTTPClient().fetch(url)
+        print('fetched %s' % url)
+
+        html = response.body if isinstance(response.body, str) \
+            else response.body.decode()
+        urls = [urljoin(url, remove_fragment(new_url))
+                for new_url in get_links(html)]
+    except Exception as e:
+        print('Exception: %s %s' % (e, url))
+        raise gen.Return([])
+
+    raise gen.Return(urls)
+
+
+def remove_fragment(url):
+    pure_url, frag = urldefrag(url)
+    return pure_url
+
+
+def get_links(html):
+    class URLSeeker(HTMLParser):
+        def __init__(self):
+            HTMLParser.__init__(self)
+            self.urls = []
+
+        def handle_starttag(self, tag, attrs):
+            href = dict(attrs).get('href')
+            if href and tag == 'a':
+                self.urls.append(href)
+
+    url_seeker = URLSeeker()
+    url_seeker.feed(html)
+    return url_seeker.urls
+
+
+@gen.coroutine
+def main():
+    q = queues.Queue()
+    start = time.time()
+    fetching, fetched = set(), set()
+
+    @gen.coroutine
+    def fetch_url():
+        current_url = yield q.get()
+        try:
+            if current_url in fetching:
+                return
+
+            print('fetching %s' % current_url)
+            fetching.add(current_url)
+            urls = yield get_links_from_url(current_url)
+            fetched.add(current_url)
+
+            for new_url in urls:
+                # Only follow links beneath the base URL
+                if new_url.startswith(base_url):
+                    yield q.put(new_url)
+
+        finally:
+            q.task_done()
+
+    @gen.coroutine
+    def worker():
+        while True:
+            yield fetch_url()
+
+    q.put(base_url)
+
+    # Start workers, then wait for the work queue to be empty.
+    for _ in range(concurrency):
+        worker()
+    yield q.join(timeout=timedelta(seconds=300))
+    assert fetching == fetched
+    print('Done in %d seconds, fetched %s URLs.' % (
+        time.time() - start, len(fetched)))
+
+
+if __name__ == '__main__':
+    import logging
+    logging.basicConfig()
+    io_loop = ioloop.IOLoop.current()
+    io_loop.run_sync(main)
+
